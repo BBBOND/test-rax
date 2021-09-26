350aef051e1bb649793920c23fbce36f0a566f10
孤燈 <weiyang.jwy@alibaba-inc.com>
2021-09-26 19:56:10 +0800

    [feature] 修改demo、

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,11 +1,12 @@
 import { createElement } from 'rax';
 import Image from 'rax-image';
+import icon from '../../static/images/icon.png';
 
 import './index.css';
 
-export default (props) => {
+export default () => {
   const source = {
-    uri: '//gw.alicdn.com/tfs/TB1MRC_cvb2gK0jSZK9XXaEgFXa-1701-1535.png',
+    uri: icon,
   };
   return (
     <Image
